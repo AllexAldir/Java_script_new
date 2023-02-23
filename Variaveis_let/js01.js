d5a82810e1fa1bd4238ddc046b82e8d31330a1e1
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-23 20:42:58 -0300

    Create js01.js

---
+++ Variaveis_let/js01.js
@@ -0,0 +1,21 @@
+//Começando com o nome:
+
+let nome = 'Állex'.toLowerCase()
+var nome_alt = 'állex'.toUpperCase()
+console.log(`${nome_alt} com letra minuscula é: ${nome}`)
+
+//não pode redeclarar variáveis!
+
+//Const:
+
+
+const nome1 = 'Állex'
+const nome2 = 'Lucas'
+
+const nome3 = nome1 + '_' + nome2 
+// concatenando para utilizar o resultado para gerar outro valor!
+console.log(nome3)
+
+
+let resul = 'Állex'
+
