422eb02557d152e8a9b4e735580e0f99443d2077
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-25 01:19:39 -0300

    Arrow Functions

---
+++ arow_function.js
@@ -0,0 +1,28 @@
+/*
+    Diferença entre arow 
+    functions e functions!
+
+*/
+
+let num = 9;
+
+
+const confirm = (num) => {
+
+    num %2 === 0
+}
+
+console.log(confirm(num));
+
+
+let a = 6
+
+let b = 7
+
+const soma = (a,b) => a+b
+
+let result = soma(a,b)
+
+console.log(`Valor do resultado é: ${result}`)
+
+//Colocando em prática o estudo de arrow functions
