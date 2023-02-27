fdb6ecfdb32e9829f0547218faabb2fbdb8ed629
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-26 22:30:19 -0300

    Estudos

---
+++ arow_functions02.js
@@ -0,0 +1,6 @@
+let num01 = 2
+let num02 = 4
+
+const soma = (num01,num02) => num01 + num02 
+
+console.log(soma(num01,num02))
---
+++ strings.js
@@ -0,0 +1,14 @@
+//Quando quiser escapar as aspas
+
+let texto = "Um \"texto\""
+
+console.log(texto)
+
+
+//Pegando o index da frase!
+
+let nome = 'Hello world!'
+
+// console.log(nome[4])
+
+console.log(nome.indexOf('world'))
