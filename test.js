11f74ef49d014e65f20167de45e18b39e18c21b8
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-28 23:50:40 -0300

    Testes

---
+++ test.js
@@ -0,0 +1,12 @@
+let num1 = 1.
+let num2 = '2'
+
+
+const result = (x) => Number.isInteger(x)
+const y = () => console.log('ok')
+
+// console.log(result(num1))
+
+if(result(num2) == false){
+    y()
+}
