816365ea1749043852b12efd821a65e8438189ad
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-03-29 00:48:28 -0300

    => Async/Await

---
+++ async_await.js
@@ -0,0 +1,16 @@
+async function Pegadados() {
+    let dado;
+    await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(e => dado = e.json())
+        .catch(e => console.log(`Erro na requisição ${e}`))
+    console.log(dado);
+}
+
+/*---------------*/
+
+async function Pgadados2() {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users').then(e => e.json())
+        .catch(e => console.log(`Erro no sistema ${e}`))
+    return(result)
+}
+
