cf81a38c4a94a9902e57e3d7d455bdb56d90031c
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-04-01 19:38:05 -0300

    => Colocando funcoes construtoras

--- funcoes_fabricas.js
+++ funcoes_fabricas.js
@@ -1,19 +1,35 @@
 //Funcoes fabrica
 const i = () => console.log('*****************')
 
-function Criapessoa(nome, sobrenome) {
+function Criapessoa(nome, sobrenome, peso = 0, altura = 0) {
     return {
         nome,
         sobrenome,
+
+        //Colocando um getter Transformando a função em um atributo
+        get NomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        //Colocando os setters
+        set NomeCompleto(valor) {
+            valor = valor.split(' ')
+            this.nome = valor.shift()
+        },
+
         Teste: function (texto) {
-            return(
+            return (
                 `A pessoa: ${this.nome} está falando sobre: ${texto}`
             )
+        },
+        peso: peso,
+        altura: altura,
+        get imc() {
+            return this.peso / (this.altura ** 2)
         }
-
     }
 }
 
-let Ob = Criapessoa('Állex','Campos')
-let resul = Ob.Teste('JavaScript')
-console.log(resul)
+let Ob = Criapessoa('luiz', 'oliveira', 73, 1.87)
+//let resul = Ob.Teste('JavaScript')
+console.log(Ob.nome); 
