ceda49df668f2cc26b974e36c992bd92e1032725
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-04-11 23:03:17 -0300

    Fix=> Consertando erro

--- besteiras/reduce.js
+++ besteiras/reduce.js
@@ -27,10 +27,22 @@ const ob = [
     { nome: 'Cicero', idade: 42 }
 ]
 
-const rs = ob.reduce((acu, val) => {
-    if (acu.idade < val.idade)
+// const rs = ob.reduce((acu, val) => {
+//     if (acu.idade < val.idade)
 
-        return val.idade
-})
+//         return val.idade
+// })
 //Tentar pegar o menor valor usando o reduce
-console.log(rs)
+//console.log(rs)
+
+
+const menor_idade = () => {
+    let menor = ob[0].idade
+    let resul;
+    for (let i in ob) {
+        let idade = ob[i].idade
+        if (idade < menor) resul = ob[i]
+    }
+    return resul
+}
+console.log(menor_idade())
