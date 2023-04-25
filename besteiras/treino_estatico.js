3ec67bae07d61cc9ad80128971f7d077426854bd
Állex <allexaldir@gmail.com>
2023-04-25 14:38:08 -0300

    Add =>
    exercicios

--- besteiras/treino_estatico.js
+++ besteiras/treino_estatico.js
@@ -20,3 +20,20 @@ pessoa.formata()
 let nome = pessoa.nome
 Pessoa.teste(nome)
 console.log(pessoa)
+
+/*----------------------------------------------------*/
+
+class Carro {
+    constructor(marca, modelo, preco) {
+        this.marca = marca
+        this.modelo = modelo
+        this.preco = preco
+    }
+
+    aumenta(x) {
+        this.preco = this.preco + (this.preco * (x / 100))
+    }
+}
+
+const carro1 = new Carro('fiat', 'uno', 15000)
+console.log(carro1)
