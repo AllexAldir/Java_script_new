740fb60141dfb56261f4fa8ad057c63f662f93be
Állex <allexaldir@gmail.com>
2023-05-03 14:06:19 -0300

    Add = >

---
+++ besteiras/ex_ajax001.js
@@ -0,0 +1,33 @@
+$('#teste01').on('click', function (e) {
+    e.preventDefault()
+
+    let href = $('#teste01').prop('html')
+    console.log(href)
+    
+    function ajax(ob) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest()
+
+            xhr.open(ob.methold, ob.url, true) //Aqui passa os parâmetros para requisição
+            xhr.send() //Caso queira envair
+
+            xhr.addEventListener('load', function () {
+                if (xhr.status >= 200 && xhr.status <= 300) {
+                    return resolve(xhr.status)
+                }
+
+                reject(new Error('erro na requisição'))
+            })
+        })
+    }
+
+    async function resultado() {
+        await ajax({ methold: 'GET', url: href })
+            .then(e => console.log(e))
+            .catch(e => console.log(e))
+    }
+
+    resultado()
+})
+
+
