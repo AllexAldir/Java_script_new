a9f5942e1b508ee31bf70c5a3cbc28a2e190e00a
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-05-07 22:43:43 -0300

    Add => HTML
    Implementação da lib adminLTE com a tabela com dados

--- fecth_api/fetch_Api_axios.js
+++ fecth_api/fetch_Api_axios.js
@@ -6,9 +6,10 @@ $(window).on('load', function () {
         //console.log(response)
         response.forEach(e => {
             //console.log(e)
-
-            $('#teste').append(`<tr><td>${e.nome}</td><td>${e.email}</td>
-            <td>${e.salario}</td><td>${e.estado}</td><td>${e.idade}</td></tr>`)
+            $('.teste').append(`<tr><td>${e.cpf}</td><td>${e.email}</td>
+            <td>${e.empresa}</td><td>${e.estado}</td><td>${e.nome}</td>
+            <td>${e.salario}</td><td>${e.idade}</td><td>${e.sexo}</td></tr>`)
+            
         });
     }
 })
