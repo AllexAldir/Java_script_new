a6e7f000d100a2a616f11a75d4ae4081c041a9b4
Allex Aldir Campos de Assis <allex.assis@grupofm.local>
2023-05-10 09:12:46 -0300

    Add => Typescript
    Adicionando eslint para correção de dos scripts

---
+++ type_script/aula,03.ts
@@ -0,0 +1,10 @@
+function calcula(x: number, y: number) {
+    return x + y
+}
+
+//console.log(calcula(3, 4))
+
+type teste = 'ok'|'teste'|'analise';
+
+const resul = (x: teste) => console.log(x);
+
